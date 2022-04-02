3a400d9bd492292963094525ceb1bc59bc6e1846
johnny-shaman <johnny.shaman.saxophonist@gmail.com>
2022-04-02 17:08:07 +0900

    Zero language implements start

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,4 @@
-mod parser;
-mod runner;
-mod node;
-use std::fs;
-
-fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 2 {
-        println!("[USAGE] zero file.zero");
-        return;
-    }
-    // ファイルを開く
-    let filename = &args[1];
-    let src = fs::read_to_string(filename).unwrap();
-    runner::run(&src);
+fn main () {
+    println!("{}", "hello")
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn test_run() {
-        assert_eq!(runner::run("say 32"), 32);
-        assert_eq!(runner::run("say 1+2*3"), 7);
-        assert_eq!(runner::run("say \"abc\""), 0);
-    }
-}
