bcc9de6fc300ed3109cc6a28bb632d6e72a2c09c
johnny-shaman <johnny.shaman.saxophonist@gmail.com>
2022-04-17 22:17:51 +0900

    modified:   src/node.rs

--- src/node.rs
+++ src/node.rs
@@ -5,40 +5,48 @@ pub enum Node {
     Logic(bool),
     Byte(char),
     Char(char),
-    Pair(Box<Node>, Box<Node>),
+
+    Pair(Box<Node>, Box<Node>)
     /*
     Apply       (Node: Lambda,  Node)   : a // Bop ?
-    List        (Node, Node: Pair)      : 
+    List        (Node, Node: Pair)      : L
     */
 
     Uop(String, Box<Node>),
     /*
-    Increment   ('++' Node)      : u
-    Decrement   ('--' Node)      : d
-    Not         ('!' Node)       : !
-    Negate      ('-' Node)       : -
+    Increment   ('++' Node)      : 'u'
+    Decrement   ('--' Node)      : 'd'
+    Not         ('!' Node)       : '!'
+    Negate      ('-' Node)       : '-'
     */
 
     Bop(Box<Node> String, Box<Node>),
     /*
-    Lambda      (Node '->' Node) : l
-    Constant    (str ':' Node)   : c
-    Separate    (Node ' ' or '/n' Node)  : s
-    Pipe        (Node '|' Node: Lambda)  : p
-    this        (Node '.' Node: Lambda)  : m
+    Cons        (Node ' ' Node)  : ','
+    Lambda      (Node '->' Node) : '\'
+    Constant    (str ':' Node)   : 'c'
+    Separate    (Node ' ' or '/n' Node)  : 's'
+    Pipe        (Node '|' Node: Lambda)  : 'p'
+    this        (Node '.' Node: Lambda)  : 'm'
+    +           (Node '+' Node)          : '+'
+    -           (Node '-' Node)          : '-'
+    *           (Node '*' Node)          : '*'
+    /           (Node '/' Node)          : '/'
+
+    
     */
 
 }
 impl Node {
-    pub fn pair(l: Node, r: Node) -> Node {
-        return Node::Pair(Box::new(l), Box::new(r));
+    pub fn Pair (l: Node, r:Node) -> Node {
+        Node::Pair(Box::new(l), Box::new(r))
     }
 
     pub fn uop(l: String, r: Node) -> Node {
-        return Node::Uop(l, Box::new(r));
+        Node::Uop(l, Box::new(r));
     }
 
-    pub fn Bop(l: Node, op: String, r: Node) -> Node {
-        return Node::Bop(op, Box::new(l), Box::new(r));
+    pub fn bop(l: Node, op: String, r: Node) -> Node {
+        Node::Bop(Box::new(l), op, Box::new(r));
     }
 }
