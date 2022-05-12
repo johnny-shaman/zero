0df00dfdeeb86f9cb7a549e0a86504aa50812543
johnny-shaman <johnny.shaman.saxophonist@gmail.com>
2022-05-12 18:20:29 +0900

    progress on parser and tokenizer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,31 +1,27 @@
 pub use self::Token::{
     Sp,
-    End,
+    Ed,
     OpPrn,
     ClPrn,
-    Comma,
-    Collon,
-    At,
-    Ident,
-    Number,
-    String,
-    Operator
+    Lt,
+    Id,
+    Pl
 };
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum Token {
     Sp,
-    End,
+    Ed,
     OpPrn,
     ClPrn,
-    Comma,
-    Collon,
-    At,
-    String(String),
-    Ident(String),
-    Number(f64),
-    Operator(String)
+    pub enum Lt {
+        N(f64),
+        S(String),
+    },
+    Id(String),
+    Pl(String)
 }
+
 
 pub fn tokenize(input: &str) -> Vec<Token> {
 
@@ -33,45 +29,39 @@ pub fn tokenize(input: &str) -> Vec<Token> {
     let preprocessed = comment_re.replace_all(input, "\n");
     let mut result = Vec::new();
 
-    let token_re = regex!(concat!(
+    let tokens = regex!(concat!(
         r"(?P<sp> +)|",
-        r"(?P<lf>\n+)|",
-        r"(?P<ident>[A-z]+\w)|",
-        r"(?P<number>\d+\.?\d*)|",
-        r"(?P<string>'.*'+)|"
-        r"(?P<at>@)|",
+        r"(?P<ed>\n+)|",
         r"(?P<oppar>\()|",
         r"(?P<clpar>\))|",
-        r"(?P<comma>,)|",
-        r"(?P<operator>\S)"
+        r"(?P<id>[A-z]+\w)|",
+        r"(?P<n>\d+\.?\d*)|",
+        r"(?P<s>'.*'+)|",
+        r"(?P<Pl>\S)"
     ));
 
-    for cap in token_re.captures_iter(preprocessed.as_str()) {
-        let token = if cap.name("ident").is_some() {
-            match cap.name("ident").unwrap() {
-                ident => Ident(ident.to_string())
+    for cap in tokens.captures_iter(preprocessed.as_str()) {
+        let token = if cap.name("id").is_some() {
+            match cap.name("id").unwrap() {
+                id => Id(ident.to_string())
             }
-        } else if cap.name("number").is_some() {
-            match cap.name("number").unwrap().parse() {
-                Ok(number) => Number(number),
-                Err(_) => panic!("Lexer failed trying to parse number")
+        } else if cap.name("n").is_some() {
+            match cap.name("n").unwrap().parse() {
+                Ok(n) => Lt::N(n),
+                Err(n) => panic!("Lexer failed trying to parse number", n)
             }
-        } else if cap.name("string").is_some() {
-            String(cap.name("string").unwrap().to_string())
+        } else if cap.name("s").is_some() {
+            Lt::S(cap.name("s").unwrap().to_string())
         } else if cap.name("sp").is_some() {
             Sp
-        } else if cap.name("lf").is_some() {
-            Lf
-        } else if cap.name("at").is_some() {
-            At
+        } else if cap.name("ed").is_some() {
+            Ed
         } else if cap.name("oppar").is_some() {
             OpPrn
         } else if cap.name("clpar").is_some() {
             ClPrn
-        } else if cap.name("comma").is_some() {
-            Comma
         } else {
-            Operator(cap.name("operator").unwrap().to_string())
+            Pl(cap.name("operator").unwrap().to_string())
         };
 
         result.push(token)
